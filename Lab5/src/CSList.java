b14a45134fc6a12c9db1b9299de5a76dfa2a651b
zablerl <zablerl@canisius.edu>
2017-10-01 17:23:45 -0400

    Just need to do remove method and sort methods. Then I need to test and debug methods.

--- Lab5/src/CSList.java
+++ Lab5/src/CSList.java
@@ -18,7 +18,7 @@ public class CSList<E> implements ICSList<E>
 			return 0;
 		}
 		run = head;
-		while (run != null)
+		while (run != tail.getNext())
 		{
 			counter++;
 			run = run.getNext();
@@ -66,7 +66,7 @@ public class CSList<E> implements ICSList<E>
 		CSNode<E> newNode = new CSNode<E>();
 		if (head.getNext() == null)
 		{
-			newNode = head.getNext();
+			head.setNext(newNode);
 			newNode.setPrev(head);
 			newNode.setNext(null);
 			newNode.setObject(e);
@@ -134,37 +134,146 @@ public class CSList<E> implements ICSList<E>
 	public void clear() 
 	{
 		// TODO Auto-generated method stub
+		head = null;
+		tail = null;
 		
 	}
 
 	@Override
-	public E get(int index) {
+	public E get(int index) throws IndexOutOfBoundsException
+	{
 		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public E set(int index, E element) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void add(int index, E element) {
-		// TODO Auto-generated method stub
+		try
+		{
+			int counter = 0;
+			if (index == 0)
+			{
+				return head.getObject();
+			}
+			run = head;
+			while (run == tail.getNext())
+			{
+				if (counter == index)
+				{
+					return run.getObject();
+				}
+				counter++;
+				run = run.getNext();
+			}
+		}
+		catch(Exception e)
+		{
+			System.out.println(e);
+		}
 		
+		return null;
 	}
 
 	@Override
-	public E remove(int index) {
+	public E set(int index, E element) throws IndexOutOfBoundsException
+	{
+		// TODO Auto-generated method stub
+		try
+		{
+			int counter = 0;
+			E temp = null;
+			if (index == 0)
+			{
+				temp = head.getObject();
+				head.setObject(element);
+				return temp;
+			}
+			run = head;
+			while (run == tail.getNext())
+			{
+				if (counter == index)
+				{
+					temp = run.getObject();
+					run.setObject(element);
+					return temp;
+				}
+				counter++;
+				run = run.getNext();
+			}
+		}
+		catch(Exception e)
+		{
+			System.out.println(e);
+		}
+		
+		return null;
+	}
+
+	
+	@Override
+	public void add(int index, E element) throws IndexOutOfBoundsException
+	{
+		// TODO Auto-generated method stub
+		try
+		{
+			int counter = 0;
+			CSNode<E> temp = new CSNode<E>();
+			CSNode<E> newNode = new CSNode<E>();
+			newNode.setObject(element);
+			if (index == 0)
+			{
+				temp = head;
+				temp.setNext(head.getNext());
+				temp.setPrev(head.getPrev());
+				head = newNode;
+				head.setPrev(null);
+				head.setNext(temp);
+				
+			}
+			run = head;
+			while (run == tail.getNext())
+			{
+				if (counter == index)
+				{
+					newNode.setPrev(run.getPrev());
+					run.getPrev().setPrev(newNode);
+					run.setPrev(newNode);
+					newNode.setNext(run);
+					
+				}
+				counter++;
+				run = run.getNext();
+			}
+		}
+		catch(Exception e)
+		{
+			System.out.println(e);
+		}
+	}
+	
+
+	@Override
+	public E remove(int index) throws IndexOutOfBoundsException
+	{
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public int indexOf(E o) {
+	public int indexOf(E o) 
+	{
 		// TODO Auto-generated method stub
-		return 0;
+		int counter = 0;
+		if (head.getObject() == o)
+		{
+			return 0;
+		}
+		run = head;
+		while (run == tail.getNext())
+		{
+			if (run.getObject() == o)
+			{
+				return counter;
+			}
+			counter++;
+			run = run.getNext();
+		}
+		return -1;
 	}
 
 	@Override
