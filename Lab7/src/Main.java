cf490ca62fc32ffe0e5ed0707c257d51b9940182
Zabler <zablerl@canisius.edu>
2017-10-18 19:46:13 -0400

    Program seems done. Needs comments and run through testing again.

---
+++ Lab7/src/Main.java
@@ -0,0 +1,90 @@
+import java.util.Scanner;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class Main
+{
+	public static void main(String[] args) throws Exception
+	{
+		String prompt = "Please enter a non-negative, non-zero integer(q to exit): ";
+		String prompt2 = "Please select an option for computation: ";
+		
+		Scanner input = new Scanner(System.in);
+		
+		ExecutorService execute = Executors.newSingleThreadExecutor();
+		
+		while (true)
+		{
+			System.out.print(prompt);
+			String integer = input.nextLine();
+			int num = 0;
+			
+			if (integer.equals("q"))
+			{
+				execute.shutdown();
+				System.exit(0);
+			}
+			
+			try
+			{
+				num = Integer.parseInt(integer);
+				
+				if (num > 0)
+				{
+					
+					while(true)
+					{
+						System.out.println(prompt2);
+						System.out.println("1. Stand-alone thread ");
+						System.out.println("2. Executor service ");
+						String request = input.nextLine();
+						
+						if (request.equals("1"))
+						{
+							Thread t = new Thread(new RunnableExample(num));
+							t.setName("Stand-alone thread");
+							System.out.println("The factors of " + integer + " are:");
+							t.start();
+							t.join();
+							break;
+						}
+						else if (request.equals("2"))
+						{
+							Future<?> task = execute.submit(new RunnableExample(num));
+							try
+							{
+								task.get();
+								break;
+							}
+							catch(ExecutionException e)
+							{
+								
+							}
+							
+						}
+						else
+						{
+							System.out.println("ERROR - invalid selection: " + request);
+							continue;
+						}
+					}
+					
+				}
+				else
+				{
+					System.out.println("ERROR- invalid intger: " + integer);
+					continue;
+				}
+			}
+			catch(Exception e)
+			{
+				System.out.println("ERROR - invalid integer: " + integer);
+			}
+			
+			
+		}
+
+	}
+}
---
+++ Lab7/src/RunnableExample.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+
+public class RunnableExample implements Runnable
+{
+	private int num;
+	
+	public RunnableExample(int num)
+	{
+		this.num = num;
+	}
+	
+	@Override
+	public void run() 
+	{
+		// TODO Auto-generated method stub
+		try
+		{
+			ArrayList<Integer> factors = new ArrayList<Integer>();
+			for (int current = 1; current <= num; current++)
+			{
+				if ((num % current) == 0)
+				{
+					factors.add(current);
+				}
+			}
+			
+			for (int i = 0; i < factors.size(); i++)
+			{
+				System.out.print(factors.get(i) + ",");
+				Thread.sleep(500);
+			}
+			System.out.println();
+		}
+		catch(Exception e)
+		{
+			
+		}
+		
+			
+		
+	}
+	
+}
